29831ae92099dc6236ca09565dc03daa003b5116
ancabvil <acabvil562@g.educaand.es>
2023-10-27 18:53:19 +0200

    java3

---
+++ unidad4/ejercicio4/src/Ejercicio4.java
@@ -0,0 +1,16 @@
+public class Ejercicio4 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Por favor, introduce el número de horas trabajadas esta semana:");
+        int horas = Integer.parseInt(System.console().readLine());
+        int sueldo;
+        if (horas > 40){
+            sueldo = horas * 16;
+            System.out.println("Tu sueldo es " + sueldo);
+        }else{
+            sueldo = horas * 12;
+            System.out.println("Tu sueldo es " + sueldo);
+            }
+
+        }
+    }
+
