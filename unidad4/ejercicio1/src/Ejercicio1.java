5e27ce36f8c818c4f797ea02caaf3021d8ed6cf7
ancabvil <acabvil562@g.educaand.es>
2023-10-27 17:38:23 +0200

    java1

--- unidad4/ejercicio1/src/Ejercicio1.java
+++ unidad4/ejercicio1/src/Ejercicio1.java
@@ -1,14 +1,27 @@
-import java.util.Scanner; 
-
 public class Ejercicio1 {
     public static void main(String[] args) throws Exception {
-        Scanner s = new Scanner(System.in)
         System.out.println("Introduzca un día de la semana:");
-        String dia= s.nextLine();
+        String respuesta = System.console().readLine();
 
-        if {
-
-            
+        switch(respuesta){
+            case "Lunes":
+            System.out.println("Tienes clase de programación");
+            break;
+            case "Martes":
+            System.out.println("Tienes clase de Bases de datos");
+            break;
+            case "Miercoles":
+            System.out.println("Tienes clase de Lenguaje de Marcas");
+            break;
+            case "Jueves":
+            System.out.println("Tienes clase de FOL");
+            break;
+            case "Viernes":
+            System.out.println("Tienes clase de Sistemas");
+            break;
+            default:
+            System.out.println("Eso no es un día de la semana");
+            break;
         }
     }
 }
