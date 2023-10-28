263783337ed761966694a40e6339f828dbbbc6d0
ancabvil <acabvil562@g.educaand.es>
2023-10-28 13:37:04 +0200

    java8

--- unidad4/Ejercicio7/src/Ejercicio7.java
+++ unidad4/Ejercicio7/src/Ejercicio7.java
@@ -11,6 +11,25 @@ public class Ejercicio7 {
         String ev3 = System.console().readLine();
         Double n3 = Double.parseDouble(ev3);
         Double fin = (n1 + n2 + n3)/3;
-        System.out.println("La media del curso es de " + fin);
+    
+        if (fin < 4.9) {
+            System.out.print("Tu media es de " + fin + " por lo que es insuficiente");
+        }
+
+        if (fin >= 5.9 && fin <= 6.9) {
+            System.out.println("Tu media es de " + fin + " por lo que es suficiente");
+        }
+        if (fin >= 7 && fin <= 7.9) {
+            System.out.println("Tu media es de " + fin + " por lo que es un bien");
+        }
+        if (fin >=8 && fin <= 9) {
+            System.out.println("Tu media es de " + fin + " por lo que es un notable");
+        }
+        if (fin >=9.1) {
+            System.out.println("Tu media es de " + fin + " por lo que es un sobresaliente");
+        }
+        }
+
+
     }
-}
+
