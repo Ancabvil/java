9ec468e60814488f99863c1263fae11b88015d94
ancabvil <acabvil562@g.educaand.es>
2023-10-29 15:51:34 +0100

    zodiaco

---
+++ unidad4/Ejercicio10/src/Ejercicio10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Ejercicio10 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Introduce tu fecha de nacimiento, el día y el mes, separados por un espacio y te digo tu horóscopo.");
+        // 12*30 360 30-21
+        
+
+//Acuario (21 Enero- 19 Febrero) 0 a 29
+//
+//Piscis (20 Febrero- 20 Marzo) 30 a 60
+
+//Aries (21 Marzo-20 Abril) 61 a 90
+
+//Tauro (21 Abril-21 Mayo) 91 a 120
+
+//Géminis (22 Mayo- 21 Junio) 121 a 150
+
+//Cáncer (22 Junio-23 Julio) 151 a 180
+
+//Leo (24 Julio- 23 Agosto) 181 a 210
+
+//Virgo (24 Agosto- 23 Septiembre) 211 a 240
+
+//Libra (24 Septiembre -23 Octubre) 241 a 270
+
+//Escorpio (24 Octubre-22 Noviembre) 271 a 300
+
+//Sagitario (23 Noviembre- 21 Diciembre) 301 a 330
+
+//Capricornio (22 Diciembre-20 Enero) 331 a 360
+}
+}
