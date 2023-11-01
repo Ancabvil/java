72229f0143a2159118cfa6c5a36a7f1d37f45745
ancabvil <acabvil562@g.educaand.es>
2023-11-01 12:22:02 +0100

    javawin

--- unidad4/Ejercicio10/src/Ejercicio10.java
+++ unidad4/Ejercicio10/src/Ejercicio10.java
@@ -1,32 +1,50 @@
-import java.util.Scanner;
 public class Ejercicio10 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Introduce tu fecha de nacimiento, el día y el mes, separados por un espacio y te digo tu horóscopo.");
-        // 12*30 360 30-21
-        
 
-//Acuario (21 Enero- 19 Febrero) 0 a 29
-//
-//Piscis (20 Febrero- 20 Marzo) 30 a 60
+  public static void main(String[] args) throws Exception {
+    System.out.println("Introduce primero el día de nacimiento");
+    String a = System.console().readLine();
+    int dia = Integer.parseInt(a);
+    System.out.println("Ahora introduzca el número de su mes de nacimiento");
+    String b = System.console().readLine();
+    int mes = Integer.parseInt(b);
 
-//Aries (21 Marzo-20 Abril) 61 a 90
+    int total = ((mes * 30) - dia) + 5;
 
-//Tauro (21 Abril-21 Mayo) 91 a 120
-
-//Géminis (22 Mayo- 21 Junio) 121 a 150
-
-//Cáncer (22 Junio-23 Julio) 151 a 180
-
-//Leo (24 Julio- 23 Agosto) 181 a 210
-
-//Virgo (24 Agosto- 23 Septiembre) 211 a 240
-
-//Libra (24 Septiembre -23 Octubre) 241 a 270
-
-//Escorpio (24 Octubre-22 Noviembre) 271 a 300
-
-//Sagitario (23 Noviembre- 21 Diciembre) 301 a 330
-
-//Capricornio (22 Diciembre-20 Enero) 331 a 360
-}
+    if (total <= 29) {
+      System.out.println("Su horoscopo es Acuario");
+    }
+    if (total >= 30 && total <= 60) {
+      System.out.println("Su Horoscopo es Piscis");
+    }
+    if (total >= 61 && total <= 90) {
+      System.out.println("Su horoscopo es Aries");
+    }
+    if (total >= 91 && total <= 120) {
+      System.out.println("Su horoscopo es Tauro");
+    }
+    if (total >= 121 && total <= 150) {
+      System.out.println("Su horoscopo es Géminis");
+    }
+    if (total >= 151 && total <= 180) {
+      System.out.println("Su horoscopo es Cáncer");
+    }
+    if (total >= 181 && total <= 210) {
+      System.out.println("Su horoscopo es Leo");
+    }
+    if (total >= 211 && total <= 240) {
+      System.out.println("Su horoscopo es Virgo");
+    }
+    if (total >= 241 && total <= 270) {
+      System.out.println("Su horoscopo es Libra");
+    }
+    if (total >= 271 && total <= 300) {
+      System.out.println("Su horoscopo es Escorpio");
+    }
+    if (total >= 301 && total <= 330) {
+      System.out.println("Su horoscopo es Sagitario");
+    }
+    if (total >= 331 && total <= 360) {
+      System.out.println("Su horoscopo es Capricornio");
+    }
+  }
 }
