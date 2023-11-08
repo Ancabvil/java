fcb3888bc45c65f8afa67b75dd72571eba4ff440
ancabvil <acabvil562@g.educaand.es>
2023-11-08 20:26:12 +0100

    java5

---
+++ unidad 5/Ejercicio4/src/Ejercicio4.java
@@ -0,0 +1,8 @@
+public class Ejercicio4 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Intervalo desde 320 a 160 de 20 en 20.");
+        for( int i = 320; i >= 160; i-=20 ){
+            System.out.println(i);
+        }
+    }
+}
