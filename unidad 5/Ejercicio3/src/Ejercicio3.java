e93a7b14556691694dcc717963bfab7d4a8de6fe
ancabvil <acabvil562@g.educaand.es>
2023-11-08 20:20:46 +0100

    java3

---
+++ unidad 5/Ejercicio3/src/Ejercicio3.java
@@ -0,0 +1,11 @@
+public class Ejercicio3 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Muestro del 5 al 100 con un do while");
+        int i = 5;
+
+        do {
+            System.out.println(i);
+            i+=5;
+        } while (i <= 100);
+    }
+}
