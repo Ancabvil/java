d0872676898d93ea4f10c926f96e287bc39a794c
ancabvil <acabvil562@g.educaand.es>
2023-11-08 20:29:20 +0100

    java5

---
+++ unidad 5/Ejercicio5/src/Ejercicio5.java
@@ -0,0 +1,12 @@
+public class Ejercicio5 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Intervalo desde 320 a 160 de 20 en 20 con while.");
+        int num = 360;
+
+        while (num >= 160) {
+            System.out.println(num);
+            num-=20;
+            
+        }
+    }
+}
