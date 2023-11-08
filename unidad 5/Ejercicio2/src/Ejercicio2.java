6087f3e20d15fb8ca3da9f61817cce002bcba11f
ancabvil <acabvil562@g.educaand.es>
2023-11-08 20:02:29 +0100

    java2

---
+++ unidad 5/Ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,11 @@
+public class Ejercicio2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Los multiplos de 5 hasta el 10 con while");
+        int i = 5;
+        while (i <= 100) {
+            System.out.println(i);
+            i+=5;
+            
+        }
+    }
+}
