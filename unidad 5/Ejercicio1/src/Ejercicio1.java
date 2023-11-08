a9b2a784e3896e031654059dc7c12ad85d48ed7d
ancabvil <acabvil562@g.educaand.es>
2023-11-08 19:55:28 +0100

    java1

---
+++ unidad 5/Ejercicio1/src/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Multiplos de 6 hasta el 100.");
+        for(int i = 5; i <= 100; i+=5){
+            System.out.println(i);
+        }
+    }
+}
