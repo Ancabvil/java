406a056688e1cee1244d300bc244894fe47c329b
ancabvil <acabvil562@g.educaand.es>
2023-11-15 17:22:47 +0100

    java11

--- unidad 5/Ejercicio11/src/Ejercicio11.java
+++ unidad 5/Ejercicio11/src/Ejercicio11.java
@@ -1,10 +1,13 @@
 public class Ejercicio11 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Le calculo cuadrado y el cubo de 5 números a partir del que introduzca");
-        System.out.print("Introduzca un número:");
-        int numero = Integer.parseInt(System.console().readLine());
-        for (int i = numero; i < (numero+5) ; i++ ){
-        System.out.printf("%4d %6d %8d\n", i , i * i, i * i * i);
-        }
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(
+      "Le calculo cuadrado y el cubo de 5 números a partir del que introduzca"
+    );
+    System.out.print("Introduzca un número:");
+    int numero = Integer.parseInt(System.console().readLine());
+    for (int i = numero; i < (numero + 5); i++) {
+      System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
     }
+  }
 }
