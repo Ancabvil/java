89723b8b4a117f290fe59d33798309fe16455263
ancabvil <acabvil562@g.educaand.es>
2023-11-18 18:46:20 +0100

    java22

---
+++ unidad 5/Ejercicio22/src/Ejercicio22.java
@@ -0,0 +1,13 @@
+public class Ejercicio22 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Números primos desde el 2 al 100, ambos incluidos.");
+        System.out.println("2");
+        for (int i = 2; i <= 100; i++){
+            if(i%2==1){
+                System.out.println(i);
+            }
+        }
+        System.out.println("100");
+        
+    }
+}
