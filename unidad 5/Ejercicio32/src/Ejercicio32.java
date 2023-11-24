ea8c5fec1dff6a3730f15df1c269b0db3d268aca
ancabvil <acabvil562@g.educaand.es>
2023-11-24 18:52:21 +0100

    java32

--- unidad 5/Ejercicio32/src/Ejercicio32.java
+++ unidad 5/Ejercicio32/src/Ejercicio32.java
@@ -1,38 +1,33 @@
 public class Ejercicio32 {
-    public static void main(String[] args) throws Exception {
-        System.out.print("Por favor, introduzca un número entero positivo: ");
-        long numero = Long.parseLong(System.console().readLine());
-        long number = numero;
-        long volteado=0;
-        int longitud=0;
-        ///if (number < 0){
-           /// System.out.println("Solo funciona con números positivos.");
-        ///}
-        if(number ==0){
-            longitud=1;
-        }
-        while (number > 0){
-            volteado = (volteado*10) + (number % 10);
-            numero /=10;
-            longitud++;
-        }
-            System.out.print("Dígitos pares: ");
-        int digitos=0;
-        int sumapares=0;
 
-        for(int i = 0; i < longitud; longitud++){
-            
-            digitos= (int)(volteado%10);
+  public static void main(String[] args) throws Exception {
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    long numeroIntroducido = Long.parseLong(System.console().readLine());
 
-            if((digitos %2)==0){
-                System.out.print(digitos + "");
-                sumapares +=digitos;
-            }
-            volteado /=10;
-        }
+    long numero = numeroIntroducido;
+    long volteado = 0;
+    int longitud = 0;
+    if (numero == 0) {
+      longitud = 1;
+    }
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+      longitud++;
+    }
+    System.out.print("Dígitos pares: ");
 
-            System.out.println("Suma de los dígitos pares: "+sumapares);
-        }
-
+    int digito;
+    int sumaPares = 0;
+    for (int i = 0; i < longitud; i++) {
+      digito = (int) (volteado % 10);
+      if ((digito % 2) == 0) {
+        System.out.print(digito + " ");
+        sumaPares += digito;
+      }
+      volteado /= 10;
     }
 
+    System.out.println("\nSuma de los dígitos pares: " + sumaPares);
+  }
+}
