60673e78149c5273f2140fa62edae5f46b87d607
ancabvil <acabvil562@g.educaand.es>
2023-12-15 11:35:05 +0100

    java

--- unidad 6/Ejercicio14/src/Ejercicio14.java
+++ unidad 6/Ejercicio14/src/Ejercicio14.java
@@ -9,7 +9,7 @@ public class Ejercicio14 {
         int minimo = 0;
 
         do{
-            numero = (int)((Math.random()*100)+1);
+            numero = (int)((Math.random()*(maximo - minimo))+ minimo);
             System.out.print("Es tu número el " + numero + " ?");
             correcto=System.console().readLine();
             correcto.toLowerCase();
@@ -20,29 +20,24 @@ public class Ejercicio14 {
             String respuesta=System.console().readLine();
             respuesta.toLowerCase();
             switch (respuesta) {
-                case "mayor":
-                    maximo = (100 - numero)-1;
-                    numero= (int)((Math.random()*maximo)+1);
-                    break;
                 case "menor":
-                    minimo = (0 + numero)+1;
-                    numero= (int)((Math.random()*minimo)+1);
+                    maximo = numero - 1;
+                    break;
+                case "mayor":
+                    minimo = numero + 1;
                     break;
                 default:
                     System.out.println("Esa respuesta no está registrada.");
                     break;
-                }    
+                }   
+                if(intentos == 0){
+                System.out.println("No le quedan intentos.");
+            } 
+            } else{
+                System.out.println("He acertado!");
             }
         }
         while (correcto.equals("no"));
-        {
-            if(correcto.equals("si") ){
-                System.out.println("He acertado!");
-            }
-            }
-            if(intentos == 0){
-                System.out.println("No le quedan intentos.");
-            }
     }
     
 }
