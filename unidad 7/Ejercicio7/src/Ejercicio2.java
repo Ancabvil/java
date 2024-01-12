e82c704ec6f72db5a7d8ab0120dc1e3c7ea2e09d
ancabvil <acabvil562@g.educaand.es>
2024-01-12 18:07:13 +0100

    java

---
+++ unidad 7/Ejercicio7/src/Ejercicio2.java
@@ -0,0 +1,30 @@
+public class Ejercicio2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Genera números y sustituye");
+
+        int val[] = new int[100];
+        for (int i=0; i < 100; i++) {
+        int num = (int)((Math.random()*20)+1);   
+            val[i]=num;
+    }
+    System.out.println("Array original");
+    for (int i : val) {
+        System.out.println(val[i]);
+    }
+        System.out.println("Introduce dos valores, el segundo sustituye al primero en el array.");
+        System.out.print("Primer valor: ");
+        int num1 = Integer.parseInt(System.console().readLine());
+        System.out.print("El segundo valor: ");
+        int num2 = Integer.parseInt(System.console().readLine());
+        for (int i : val) {
+        if (val[i] == num1){
+            val[i]=num2;
+        }
+    }
+    
+    for (int i : val) {
+        System.out.println(val[i]);    
+    }
+    
+    }
+}
