a66259a94372aa6a71535c956fa2317f8c88a0a9
Ancabvil <146119636+Ancabvil@users.noreply.github.com>
2024-02-21 21:11:02 +0100

    mensajito

--- Unidad10/Ejercicio2/src/Ejercicio2.java
+++ Unidad10/Ejercicio2/src/Ejercicio2.java
@@ -3,7 +3,7 @@ public class Ejercicio2 {
     public static void main(String[] args) throws Exception {
         System.out.println("ArrayList de numeros aleatorios");
         ArrayList<Integer> numero = new ArrayList<>();
-        int aleatoriosPos = (int)(Math.random()*(20-10)+10);
+        int aleatoriosPos = (int)(Math.random()*((20-10)+10)+1);
 
         for ( int i = 0; i < aleatoriosPos; i++  ) {
             int aleatoriosNum = (int)(Math.random()*100+1);
